215cc07ad7dd84e0894533a70c297521564f41f1
gradan-hash <mutukunyaa@gmail.com>
2024-02-14 08:22:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB);
+    console.log("database connection online");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.use(cors());
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "something went wrong";
+
+  return res.status(errorStatus).send(errorMessage);
+});
+
+app.listen(4000, () => {
+  connect();
+  console.log("backend running");
+});
